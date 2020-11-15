4ff93c4323f4e8ecc08ec2a3ba8ff9ec60a1596c
SamuelR <s_ramirezg_0624@outlook.com>
2020-11-15 00:03:16 -0600

    Release version 1.1 of the project

--- config/voyager.php
+++ config/voyager.php
@@ -145,8 +145,8 @@ return [
 
         'widgets' => [
             'TCG\\Voyager\\Widgets\\UserDimmer',
-            'TCG\\Voyager\\Widgets\\PostDimmer',
-            'TCG\\Voyager\\Widgets\\PageDimmer',
+            'App\\Widgets\\Leads',
+            'App\\Widgets\\Steps',
         ],
 
     ],
