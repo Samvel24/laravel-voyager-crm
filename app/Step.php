4f0dffa4324a889139a1c692c84e48c722845ae4
SamuelR <s_ramirezg_0624@outlook.com>
2020-12-05 02:39:04 -0600

    Bumped version number to 1.2.3; Se agregaron validadores. Se actualizo la documentacion de API

--- app/Step.php
+++ app/Step.php
@@ -4,9 +4,44 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+* Class Step
+* @package App
+*
+* @OA\Tag(
+*   name="Step",
+*   description="Step Operations"
+* )
+*
+* @OA\Schema(
+*     title="Step",
+*     description="Step model",
+*     @OA\Xml(
+*         name="Step"
+*     )
+* )
+*/
+
 class Step extends Model
 {
     //
     public $timestamps = false;
     protected $fillable = ['key', 'value'];
+
+    /**
+    *@OA\Property(
+    *   property="key",
+    *   title="Step Key",
+    *   description="Step Key",
+    *   type="string",
+    *   example="abierto"
+    * ),
+    *@OA\Property(
+    *   property="value",
+    *   title="Step Value",
+    *   description="Step Value",
+    *   type="string",
+    *   example="Abierto"
+    *)
+    */    
 }
